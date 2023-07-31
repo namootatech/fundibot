acbc14e357293694e4fe319375d7394d40750957
Ayabonga Qwabi <aya@simply.co.za>
2023-07-31 11:42:25 +0200

    refactor: add api router to get institutions by creator

---
+++ src/pages/api/institutions/get-by-creator/index.js
@@ -0,0 +1,38 @@
+import clientPromise from "@/lib/mongo";
+import logger from "@/lib/logger";
+import { ObjectId } from "mongodb";
+
+export default async function handler(req, res) {
+  try {
+    if (req.method === "GET") {
+      // Process a POST request
+      const client = await clientPromise;
+      const db = client.db("development");
+      logger.info(req.query);
+      logger.info(req.query.page);
+      const page = parseInt(req.query.page, 10);
+      const creator = req.query.creator;
+      logger.info(
+        `** Fetching institutions page created by:${creator} ,page: ${page}`
+      );
+      const limit = 10;
+      const skip = page * limit;
+      const institutions = await db
+        .collection("institutions")
+        .find({ "createdBy.id": { $eq: creator } })
+        .skip(skip)
+        .limit(limit)
+        .toArray();
+
+      const total = institutions.length;
+      logger.info(`** Found , ${total},  institutions`);
+      res.json({ institutions, total });
+    } else {
+      logger.info("** Method not allowed ", req.method);
+      res.status(405).json({ message: "Method not allowed" });
+    }
+  } catch (e) {
+    logger.error(e);
+    res.status(405).json({ message: "Something went wrong", error: e });
+  }
+}
