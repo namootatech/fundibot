d25fa7e49d0f33f7fb37bf2cfd5ebe3b87b1a1c0
Ayabonga Qwabi <aya@simply.co.za>
2023-08-14 20:08:27 +0200

    enable styled components ssr

--- next.config.js
+++ next.config.js
@@ -7,6 +7,16 @@ const nextConfig = {
   publicRuntimeConfig: {
     NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
   },
+  compiler: {
+    styledComponents: {
+      // Enable display of the component name along with the generated className (needed for debugging).
+      displayName: true,
+      // Enable SSR support
+      ssr: true,
+      // Optional
+      fileName: false,
+    },
+  },
 };
 
 module.exports = nextConfig;
