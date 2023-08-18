c020afcdd80bf79a3d1cfcbf535498530f4bc0e4
Ayabonga Qwabi <aya@simply.co.za>
2023-08-18 10:27:12 +0200

    fix: styled components

--- next.config.js
+++ next.config.js
@@ -8,14 +8,8 @@ const nextConfig = {
     NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
   },
   compiler: {
-    styledComponents: {
-      // Enable display of the component name along with the generated className (needed for debugging).
-      displayName: true,
-      // Enable SSR support
-      ssr: true,
-      // Optional
-      fileName: false,
-    },
+    reactStrictMode: true,
+    styledComponents: true,
   },
 };
 
